e9c0727609513ecd0d0f9e2bf12c2a1ef5585d70
HamzaMubarik <meharhamza288@gmail.com>
2024-07-04 20:04:31 +0500

    password valid

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -78,7 +78,7 @@ function Signup() {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                        placeholder='Password'
+                        placeholder='Password Should 6 integer long'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
