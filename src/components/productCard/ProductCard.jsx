7d6580731f6e4a4bbf21265e68098c3d126a5b0c
HamzaMubarik <meharhamza288@gmail.com>
2024-07-04 14:22:20 +0500

    updated

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -26,7 +26,7 @@ function ProductCard() {
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-8 md:py-16 mx-auto">
                 <div class="lg:w-1/2 w-full mb-6 lg:mb-10">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Our Latest Collection</h1>
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>All Seasoned Fruits</h1>
                     <div class="h-1 w-20 bg-purple-400 rounded"></div>
                 </div>
 
